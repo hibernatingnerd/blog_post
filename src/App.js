5476a531efcfcb9117f1a5ab7c36e81772712f26
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-08-22 15:29:03 -0400

    add onClick for running a method to change post body. Add onChange for running an helper method to update post body text as user types.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,46 @@ import Comment from './Comment.js';
 import Author from './Author.js';
 
 class Post extends Component {
-  render() {
-    let authors = this.props.allAuthors.map( (author, index) => (
-      <Author author={author} key={index} />
-    ))
+  constructor (props) {
+  //make a call to parent
+  super()
+  //  define an initial state
+  this.state = {
+    body: props.body
+  }
+}
+
+changeBody(e) {
+  let newBody = "hey you!"
+  this.setState({
+    body: newBody
+  })
+}
+
+handleFormInput (e) {
+  console.log(e);
+  this.setState({
+    body: e.target.value
+  })
+}
+
+
+
+
+
+render() {
+  let authors = this.props.allAuthors.map( (author, index) => (
+    <Author author={author} key={index} />
+  ))
 
     return (
       <div>
         <h1>{this.props.title}</h1>
         {authors}
         <div>
-          <p>{this.props.body}</p>
+          <p>{this.state.body}</p>
+          <input type="text" onChange={(e) => this.handleFormInput(e)}></input>
+          <button onClick={(e) => this.changeBody(e)}>Edit Body</button>
         </div>
         <h3>Comments:</h3>
         <Comment body={this.props.comments[0]} />
